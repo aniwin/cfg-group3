ddbff2444667bb831b9fff0761ee7de35561c0b3
hanidore <hanidore@hotmail.co.uk>
2022-10-20 22:41:16 +0100

    Try and Except for game menu where input invalid or if number if out of range.

--- Project/game_menu.py
+++ Project/game_menu.py
@@ -4,17 +4,22 @@ from main_menu import *
 
 def game_menu():
     print("GAMES")
-    choice = input("1: Rock👊 Paper✋ Scissors✌\n2: Get my weather advice\n3: Return to main menu\nPlease choose an option: ")
+while True:
+    try:
+        choice = int(input("1: Rock👊 Paper✋ Scissors✌\n2: Get my weather advice\n3: Return to main menu\nPlease choose an option: "))
 
-    if choice == "1":
-        rock_paper_scissors_menu()
-    elif choice == "2":
-        weather_advisor()
-    elif choice == "3":
-        main_menu()     # link back to main menu
+        if choice == "1":
+            rock_paper_scissors_menu()
+        elif choice == "2":
+            weather_advisor()
+        elif choice == "3":
+            main_menu()     # link back to main menu
+    except:
+        print("invalid input")
     else:
-        print("Invalid input. Enter 1, 2 or 3!")
-        game_menu()
-
+        if 1 <= choice <= 3:
+            break
+        else:
+            print("Number out of range")
 if __name__ == "__main__":
     game_menu()
