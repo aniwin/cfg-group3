344f2e655a12702df58a9471f25a9d0964da45a4
elizabethmillard <ejmillard@icloud.com>
2022-10-24 13:34:02 +0100

    added object for sleep and updated sleep function to reflect this

--- Project/classes.py
+++ Project/classes.py
@@ -7,13 +7,13 @@
 # find snow level
 
 class Pet():
-    def __init__(self, name, gender):
+    def __init__(self, name, gender, sleep):
         self.name = name
         self.gender = gender
         self.happiness = 0
         self.hunger = 0
         self.findsnow = False
-        self.sleep = False
+        self.sleep = sleep
 
 
     def update_name(self, name):
@@ -28,6 +28,11 @@ class Pet():
     def get_gender(self):
         return self.gender
 
+    def is_asleep(self):
+        self.sleep = True
+
+    def is_not_asleep(self):
+        self.sleep = False
 
 # functions to update int values for happiness and hunger
 # understand how to return a True value for find snow
