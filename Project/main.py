728b5edefba4dafee6f7581d736cc4c0b13d7602
Julie Thomas <102959256+juliethomas82@users.noreply.github.com>
2022-10-25 12:56:39 +0100

    When running main, auto running create_stats_file.py and auto installing requirements for game

--- Project/main.py
+++ Project/main.py
@@ -1,4 +1,18 @@
 from opening_menu import *
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+with open('requirements.txt') as file:
+    for line in file.readlines():
+        install(line)
+def run(runfile):
+    with open(runfile, "r") as rnf:
+        exec(rnf.read())
+
+run("create_stats_file.py")
 
 
 if __name__ == "__main__":
